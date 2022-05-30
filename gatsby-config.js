aaf8b28c8bcef55944940ca180e446466576e833
t4y3 <y.teraoka8@gmail.com>
2022-05-30 14:18:32 +0900

    fix: netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,15 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify`,
+    {
+      resolve:`gatsby-plugin-netlify`,
+      options: {
+        headers: {
+          '/using-dsg': [
+            'cache-control: private, max-age=0, must-revalidate'
+          ]
+        }
+      }
+    }
   ],
 }
